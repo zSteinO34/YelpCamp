a29c769a5677289fd4e85d9efbb210811dd0ce20
zSteinO34 <44076846+zSteinO34@users.noreply.github.com>
2019-07-09 10:47:59 -0500

    campground model final update

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,11 @@ var mongoose = require("mongoose");
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
-   location: String,
    image: String,
    description: String,
+   location: String,
+   lat: Number,
+   lng: Number,
    author: {
       id: {
          type: mongoose.Schema.Types.ObjectId,
@@ -15,7 +17,17 @@ var campgroundSchema = new mongoose.Schema({
    comments: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Comment"
-        }]
+        }],
+   reviews: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Review"
+      }
+   ],
+   rating: {
+      type: Number,
+      default: 0
+   }
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
